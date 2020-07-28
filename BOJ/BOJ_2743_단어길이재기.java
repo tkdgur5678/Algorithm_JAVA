f6bc1be01acd17f837eb9b340b5162df4f58057b
tkdgur5678 <41055954+tkdgur5678@users.noreply.github.com>
2020-07-28 23:14:25 +0900

    feat: Solved BOJ 2743

---
+++ BOJ/BOJ_2743_단어길이재기.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class BOJ_2743_단어길이재기 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.next().length());
+	}
+
+}

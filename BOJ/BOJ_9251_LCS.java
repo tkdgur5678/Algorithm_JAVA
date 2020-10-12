6872f7f5c67169e6f94625394d908fda4d6fa1e7
tkdgur5678 <41055954+tkdgur5678@users.noreply.github.com>
2020-10-12 22:43:54 +0900

    Update BOJ_9251_LCS.java

--- BOJ/BOJ_9251_LCS.java
+++ BOJ/BOJ_9251_LCS.java
@@ -1,34 +1,29 @@
+import java.io.*;
 import java.util.*;
-
-public class BOJ_9251_LCS {
-	static String a,b;
-	
-	static int dp[][];
-	
-	
-	public static void main(String[] args)   {
-		Scanner sc = new Scanner(System.in);
-		
-		a=sc.nextLine();
-		b=sc.nextLine();
-		
-		dp = new int[b.length()+1][a.length()+1]; 
-		
-		for(int i=1;i<=b.length();i++) { 
-			
-			for(int j=1;j<=a.length();j++) { 
-				
-				if(b.charAt(i-1) != a.charAt(j-1)) { 
-					
-					dp[i][j] = Math.max(dp[i][j-1],dp[i-1][j]); 
-				}else { 
-					dp[i][j] = dp[i-1][j-1]+1;
-				}
-				
-			}		
-			
-		}
-		
-		System.out.println(dp[b.length()][a.length()]);
-	}
+ 
+public class Main
+{
+    static int[][] dp = new int[1001][1001];
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String a = br.readLine();
+        String b = br.readLine();
+        for(int i=0; i<a.length(); i++)
+        {
+            for(int j=0; j<b.length(); j++)
+            {
+                if(a.charAt(i) == b.charAt(j))
+                {
+                    dp[i+1][j+1] = dp[i][j]+1;
+                }
+                else
+                {
+                    dp[i+1][j+1] = Math.max(dp[i][j+1], dp[i+1][j]);
+                }
+            }
+        }
+        System.out.println(dp[a.length()][b.length()]);
+    }
 }
